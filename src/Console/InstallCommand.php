fd2293242cfada93bf1366ed6d7b567a65961e40
wab-gl <102791235+wab-gl@users.noreply.github.com>
2026-01-22 20:53:08 +0500

    [Config updated]

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -4,6 +4,7 @@ namespace GreeLogix\RequestLogger\Console;
 
 use GreeLogix\RequestLogger\Http\Middleware\LogRequests;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class InstallCommand extends Command
 {
@@ -12,7 +13,7 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'gl-request-logger:install';
+    protected $signature = 'gl-request-logger:install {--force : Force overwrite existing config file}';
 
     /**
      * The console command description.
@@ -27,7 +28,29 @@ class InstallCommand extends Command
     public function handle(): int
     {
         $this->info('Publishing configuration...');
-        $this->callSilent('vendor:publish', ['--tag' => 'gl-request-logger-config']);
+        
+        $configPath = config_path('gl-request-logger.php');
+        $configExists = File::exists($configPath);
+        
+        if ($configExists && !$this->option('force')) {
+            // Check if config has the new options
+            $existingConfig = require $configPath;
+            $hasNewOptions = isset($existingConfig['ui_middleware']) && isset($existingConfig['allowed_emails']);
+            
+            if (!$hasNewOptions) {
+                $this->warn('Config file exists but is missing new options (ui_middleware, allowed_emails).');
+                $this->info('Updating config file with new options...');
+                $this->updateConfigFile($configPath, $existingConfig);
+            } else {
+                $this->info('Config file already exists and is up to date.');
+            }
+        } else {
+            // Publish config (will overwrite if --force is used)
+            $this->callSilent('vendor:publish', [
+                '--tag' => 'gl-request-logger-config',
+                '--force' => $this->option('force'),
+            ]);
+        }
 
         $bootstrapApp = base_path('bootstrap/app.php');
 
@@ -43,5 +66,41 @@ class InstallCommand extends Command
         $this->line('Route: GET /gl/request-logs (web middleware) to view logs.');
 
         return self::SUCCESS;
+    }
+
+    /**
+     * Update existing config file with new options.
+     */
+    protected function updateConfigFile(string $configPath, array $existingConfig): void
+    {
+        // Read the package's default config file as a string
+        $packageConfigPath = __DIR__.'/../../config/gl-request-logger.php';
+        $packageConfigContent = File::get($packageConfigPath);
+        
+        // Extract the new options section (UI Middleware and Allowed Emails)
+        // Match from the comment to the closing bracket and comma
+        if (preg_match('/(\/\*\s*UI Middleware.*?\*\/\s*\'ui_middleware\'.*?\[.*?\],\s*\/\*\s*Allowed Emails.*?\*\/\s*\'allowed_emails\'.*?\[.*?\],)/s', $packageConfigContent, $matches)) {
+            $newOptions = $matches[1];
+            
+            // Read existing config file
+            $existingContent = File::get($configPath);
+            
+            // Remove the closing bracket and any trailing whitespace
+            $existingContent = rtrim($existingContent);
+            if (substr($existingContent, -2) === '];') {
+                $existingContent = rtrim(substr($existingContent, 0, -2));
+            }
+            
+            // Append new options before the closing bracket
+            $updatedContent = $existingContent . ",\n\n" . $newOptions . "\n];\n";
+            
+            File::put($configPath, $updatedContent);
+            $this->info('Config file updated successfully with new options (ui_middleware, allowed_emails)!');
+        } else {
+            $this->warn('Could not extract new options from package config. Please add them manually.');
+            $this->line('Add these options to your config file:');
+            $this->line('  - ui_middleware');
+            $this->line('  - allowed_emails');
+        }
     }
 }
